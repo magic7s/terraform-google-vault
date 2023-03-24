d12a64be32d5fc0eab5216e8fc44c878f937e5fb
Brad Downey <bdowney@gitlab.com>
2023-03-24 10:38:25 -0700

    update url on docker hub add login

--- push_vault_to_gcp.sh
+++ push_vault_to_gcp.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 # https://hub.docker.com/_/vault/?tab=tags&page=1&ordering=last_updated
-VAULT_IMAGE="vault:1.7.2"
+VAULT_IMAGE="hashicorp/vault:1.7.2"
 GCP_PROJECT="vault-cloud-run-2020-05-04"
 GCP_REGION="us.gcr.io"
 GCP_IMAGE="$GCP_REGION/$GCP_PROJECT/$VAULT_IMAGE"
 
 docker image pull $VAULT_IMAGE
 docker image tag $VAULT_IMAGE $GCP_IMAGE
+docker login -u _json_key -p $GOOGLE_CREDENTIALS https://$GCP_REGION
 docker push $GCP_IMAGE
 
 echo "$GCP_IMAGE"
